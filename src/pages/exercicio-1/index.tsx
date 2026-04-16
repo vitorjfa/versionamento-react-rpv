daf92d80023d1d85cd77b174e1b4ea8258711e6c
vitorjfa <vitorfernandesamorim@gmail.com>
2026-04-16 20:02:10 -0300

    feat: exercicio 3 - estilização

--- src/pages/exercicio-1/index.tsx
+++ src/pages/exercicio-1/index.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
+import { text } from "stream/consumers";
+
 export default function Ex01() {
+  const [tema, setTema] = useState<boolean>(false);
+
   return (
     <>
-      <h1>Exercicio 01</h1>
-      <div>
-        <p>Este é o exercício 01</p>
+      <div
+        className={`flex flex-col justify-center items-center min-h-screen gap-20
+                ${tema ? "bg-gray-300" : "bg-black"}`}
+      >
+        <h1 className={`${tema ? "text-black" : "text-zinc-100"}`}>
+          Exercicio 01
+        </h1>
+
+        <button
+          className={`flex justify-center items-center cursor-pointer ${tema ? "text-black" : "text-zinc-100"}`}
+          onClick={() => setTema(!tema)}
+        >
+          Trocar Tema
+        </button>
       </div>
     </>
   );
